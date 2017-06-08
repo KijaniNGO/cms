25daa087dab5bbd1bcbb61ee61ea860d96d2a63e
MrLoh <flojoto@hotmail.com>
2017-06-08 16:11:28 +0200

    add antd theme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import enUS from 'antd/lib/locale-provider/en_US'
+import LocaleProvider from 'antd/lib/locale-provider'
+import { DatePicker, message } from 'antd'
 
-const App = () => <div>Hello World</div>
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: '',
+        }
+    }
+    handleChange(date) {
+        message.info(`Selected Date: ${date && date.toString()}`)
+        this.setState({ date })
+    }
+    render() {
+        return (
+            <div style={{ width: 400, margin: '100px auto' }}>
+                <DatePicker onChange={value => this.handleChange(value)} />
+                <div style={{ marginTop: 20 }}>Date: {this.state.date && this.state.date.toString()}</div>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<LocaleProvider locale={enUS}><App /></LocaleProvider>, document.getElementById('root'))
 registerServiceWorker()
