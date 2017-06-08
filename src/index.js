ad76467a8c194d7c789a52b66940f51578677c43
MrLoh <flojoto@hotmail.com>
2017-06-08 16:25:25 +0200

    add now deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import enUS from 'antd/lib/locale-provider/en_US'
 import LocaleProvider from 'antd/lib/locale-provider'
-import { DatePicker, message } from 'antd'
+import 'typeface-merriweather-sans'
+
+import DatePicker from 'antd/lib/date-picker'
+import message from 'antd/lib/message'
 
 class App extends React.Component {
     constructor(props) {
@@ -26,5 +29,11 @@ class App extends React.Component {
     }
 }
 
-ReactDOM.render(<LocaleProvider locale={enUS}><App /></LocaleProvider>, document.getElementById('root'))
+ReactDOM.render(
+    <LocaleProvider locale={enUS}>
+        <App />
+    </LocaleProvider>,
+    document.getElementById('root'),
+)
+
 registerServiceWorker()
